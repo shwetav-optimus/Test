6e125531e140767da0545b001a84f924b4e4ebb2
root <root@Optimus-17.(none)>
2014-09-15 17:32:18 +0530

    initial commit

--- features/step_definitions/step_GoogleLogin.rb
+++ features/step_definitions/step_GoogleLogin.rb
@@ -6,19 +6,16 @@ Given /^I navigate to the website$/ do
 end
 
 When /^I fill in the Username$/ do
-
+@browser.text_field(:name, 'Email').set 'shweta.verma@optimusinfo.com'
 end
 
 And /^I fill in the Password$/ do
-
+@browser.text_field(:name, 'Passwd').set '123456'
 end
 
-And /^I click on Login$/ do
-
+Then /^I click on SignIn$/ do
+@browser.button(:name, 'signIn').click
 end
 
-Then /^I should be logeed in$/ do
-
-end
 
 
